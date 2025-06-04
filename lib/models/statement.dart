b9f513fef24c92c917bc60a3bad04046a1a90eb4
InterestelarStudios <interestelarstd@gmail.com>
2025-06-03 20:45:41 -0400

    statement model created

---
+++ lib/models/statement.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:safe_driver_core/models/lideranca.dart';
+import 'package:safe_driver_core/models/usuario.dart';
+
+class Statement {
+
+  String id;
+  String messege;
+  LiderancaDetails lideranca;
+  UserDetails creator;
+  Timestamp created;
+
+  Statement({
+    required this.id,
+    required this.messege,
+    required this.lideranca,
+    required this.creator,
+    required this.created,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'messege': messege,
+      'lideranca': lideranca.toMap(),
+      'creator': creator.toMap(),
+      'created': created,
+    };
+  }
+
+  factory Statement.fromMap(Map<String, dynamic> map) {
+    return Statement(
+      id: map['id'] as String,
+      messege: map['messege'] as String,
+      lideranca: LiderancaDetails.fromMap(map['lideranca'] as Map<String,dynamic>),
+      creator: UserDetails.fromMap(map['creator'] as Map<String,dynamic>),
+      created: map['created'] as Timestamp,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Statement.fromJson(String source) => Statement.fromMap(json.decode(source) as Map<String, dynamic>);
+}
